7736745b5afdaee88b2dcd67a4fe8604c3852a0c
AleksMaifet <aleksmaifet@gmail.com>
2021-11-29 11:14:25 +0300

    Redactor code

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,33 @@
-import React, {useCallback, useEffect} from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
-import {Todolist} from "./TodoList";
-import {AddItemForm} from "./components/AddItemForm";
-import {Container, Grid, Paper} from "@material-ui/core";
-import {createTodolistTC, setTodolistsTC} from "./reducer/TodoListReducer";
-import {useDispatch, useSelector} from "react-redux";
-import {error, todolistsState} from "./Selectors/Selectors";
-import {ErrorAlert} from "./components/ErrorAlert";
+import {Container} from "@material-ui/core";
+import {setTodolistsTC} from "./reducer/TodoListReducer";
+import {useDispatch} from "react-redux";
 import MenuAppBar from "./components/AppBar";
 import {LinearIndeterminate} from "./components/LinearProgress";
+import {TodolistDemo} from "./components/TodolistDemo";
+import {Routes,Route} from "react-router-dom";
+import {AuthLogin} from "./components/AuthLogin/AuthLogin";
 
 
 function App() {
 	const dispatch = useDispatch();
-	const todolist = useSelector(todolistsState);
-	const errorStatus = useSelector(error)
+
 
 	useEffect(() => {
 		dispatch(setTodolistsTC())
 	}, [dispatch])
 
 
-	const addTodolist = useCallback( async (todolistTitle: string) => {
-		dispatch(createTodolistTC(todolistTitle))
-	},[dispatch])
-
-
-	const todolistsComponents = todolist.map(tl => {
-		return (
-			<Grid key={tl.id} item>
-				<Paper elevation={3} style={{padding: '20px'}}>
-					<Todolist
-						key={tl.id}
-						id={tl.id}
-					/>
-				</Paper>
-			</Grid>
-		)
-	})
 	return (
-				<Container fixed>
-					<MenuAppBar/>
-					<LinearIndeterminate/>
-					<div className='App'>
-						<Grid container style={{margin: '50px 0', justifyContent: 'center'}}>
-							<AddItemForm callBack={addTodolist}/>
-						</Grid>
-						<Grid container spacing={6}>
-							{todolistsComponents}
-						</Grid>
-						<ErrorAlert errorStatus={errorStatus}/>
-					</div>
-				</Container>
+		<Container fixed>
+			<MenuAppBar/>
+			<LinearIndeterminate/>
+			<Routes>
+				<Route path="/Todolist" element={<TodolistDemo/>}/>
+				<Route path="/login" element={<AuthLogin/>}/>
+			</Routes>
+		</Container>
 	);
 }
 
